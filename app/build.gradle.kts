13531a867a58b2554121f47ddca7452008b7e34d
Adriano <adrianolcx@gmail.com>
2025-08-29 13:17:03 +0200

    Install Splashscreen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.splashscreen)
 
     implementation(libs.hilt.android)
     ksp(libs.hilt.compiler)
--- app/src/main/java/com/example/catcompose/MainActivity.kt
+++ app/src/main/java/com/example/catcompose/MainActivity.kt
@@ -1,6 +1,13 @@
 package com.example.catcompose
 
+import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+        super.onCreate(savedInstanceState)
+    }
 }
