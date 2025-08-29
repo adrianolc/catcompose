69e6abbb7e48c95a893e4bd01c3e7aedb6924cc3
Adriano <adrianolcx@gmail.com>
2025-08-29 16:19:30 +0200

    Call list service api

--- features/list/src/main/java/com/example/catcompose/features/list/ui/ListViewModel.kt
+++ features/list/src/main/java/com/example/catcompose/features/list/ui/ListViewModel.kt
@@ -1,8 +1,13 @@
 package com.example.catcompose.features.list.ui
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.catcompose.features.list.api.ListService
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -10,5 +15,13 @@ class ListViewModel @Inject constructor(
     private val listService: ListService,
 ) : ViewModel() {
 
+    private val _state = MutableStateFlow<ListViewState>(ListViewState.Loading)
+    val state: StateFlow<ListViewState> = _state.asStateFlow()
 
+    init {
+        viewModelScope.launch {
+            val cats = listService.getCats(10).map { Cat(it.id, it.url) }
+            _state.value = ListViewState.Success(cats)
+        }
+    }
 }
