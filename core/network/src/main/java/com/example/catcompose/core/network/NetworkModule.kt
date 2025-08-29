fbe445889e16803e54f415ed0a3938b2cfc16c9b
Adriano <adrianolcx@gmail.com>
2025-08-29 13:32:25 +0200

    Install OkHttp

--- core/network/src/main/java/com/example/catcompose/core/network/NetworkModule.kt
+++ core/network/src/main/java/com/example/catcompose/core/network/NetworkModule.kt
@@ -1,4 +1,11 @@
 package com.example.catcompose.core.network
 
-class NetworkModule {
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal object NetworkModule {
+
 }
