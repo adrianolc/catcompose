d76205aeb348f4496c29247ab49eef2cac75818f
Adriano <adrianolcx@gmail.com>
2025-08-30 14:54:47 +0200

    Install compose preview

--- features/list/build.gradle.kts
+++ features/list/build.gradle.kts
@@ -69,6 +69,9 @@ dependencies {
     implementation(libs.coil.compose)
     implementation(libs.coil.network)
 
+    implementation(libs.androidx.compose.ui.tootling.preview)
+    debugImplementation(libs.androidx.compose.ui.tootling)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
