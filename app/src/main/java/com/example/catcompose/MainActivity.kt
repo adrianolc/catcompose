77a6578b224fb1ded279946f96ae4a348fb6f376
Adriano <adrianolcx@gmail.com>
2025-08-30 18:47:24 +0200

    Add hilt annotation to MainActivity

--- app/src/main/java/com/example/catcompose/MainActivity.kt
+++ app/src/main/java/com/example/catcompose/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.catcompose.ui.CatApp
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
