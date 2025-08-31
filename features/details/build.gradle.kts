f1f384de6dd16fc55d9cb148683f1d32c945a70c
Adriano <adrianolcx@gmail.com>
2025-08-31 19:04:45 +0200

    Add compose and viewmodel dependencies

--- features/details/build.gradle.kts
+++ features/details/build.gradle.kts
@@ -3,9 +3,10 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.hilt.android)
     alias(libs.plugins.ksp)
-    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.compose)
 }
 
 android {
@@ -41,10 +42,19 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.lifecycle.viewModelCompose)
     implementation(libs.material)
+
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.foundation)
+    implementation(libs.androidx.lifecycle.runtimeCompose)
+    implementation(libs.androidx.hilt.navigation.compose)
+
+    implementation(libs.androidx.navigation.compose)
 
     implementation(libs.retrofit)
     implementation(libs.kotlinx.serialization.json)
--- features/details/src/main/java/com/example/catcompose/features/details/ui/DetailsViewModel.kt
+++ features/details/src/main/java/com/example/catcompose/features/details/ui/DetailsViewModel.kt
@@ -1,11 +1,13 @@
 package com.example.catcompose.features.details.ui
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.catcompose.core.network.NetworkResult
 import com.example.catcompose.features.details.repo.DetailsRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 internal class DetailsViewModel @Inject constructor(
