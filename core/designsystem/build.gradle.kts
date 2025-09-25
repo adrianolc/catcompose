2abfb06800a77039d2a8ebe29865e0eb4960cdfc
Adriano <adrianolcx@gmail.com>
2025-09-25 10:57:18 +0200

    Add design system module

---
+++ core/designsystem/build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    alias(libs.plugins.catcompose.android.library)
+    alias(libs.plugins.catcompose.compose)
+}
+
+android {
+    namespace = "com.example.catcompose.core.designsystem"
+}
