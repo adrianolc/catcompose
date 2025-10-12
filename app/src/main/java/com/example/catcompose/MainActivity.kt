65c9cbb132f5cb622f90a07b6e483ef06f155c2c
Adriano <adrianolcx@gmail.com>
2025-10-12 12:37:16 +0200

    Add auto merge

--- app/src/main/java/com/example/catcompose/MainActivity.kt
+++ app/src/main/java/com/example/catcompose/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.catcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.MaterialTheme
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.catcompose.core.designsystem.CatTheme
 import com.example.catcompose.ui.CatApp
