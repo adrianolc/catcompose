c37553b6b177c8fb13faf2d4bf50b26c5539a7f4
Adriano <adrianolcx@gmail.com>
2025-10-14 14:04:26 +0200

    Add ListViewModelTest

--- features/list/src/test/java/com/example/catcompose/features/list/ui/ListViewModelTest.kt
+++ features/list/src/test/java/com/example/catcompose/features/list/ui/ListViewModelTest.kt
@@ -1,3 +1,57 @@
 package com.example.catcompose.features.list.ui
 
-class ListViewModelTest
+import app.cash.turbine.test
+import com.example.catcompose.core.network.NetworkResult
+import com.example.catcompose.core.test.TestCoroutineRule
+import com.example.catcompose.features.list.repo.ListRepository
+import io.kotest.matchers.shouldBe
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.junit.Rule
+import org.junit.Test
+
+class ListViewModelTest {
+    @get:Rule
+    val coroutineRule = TestCoroutineRule()
+
+    private lateinit var viewModel: ListViewModel
+    private val listRepository: ListRepository = mockk(relaxed = true)
+
+    @Test
+    fun `should show loading state`() =
+        runTest {
+            viewModel = ListViewModel(listRepository)
+
+            viewModel.viewState.test {
+                awaitItem() shouldBe ListViewState.Loading
+            }
+        }
+
+    @Test
+    fun `should load cats`() =
+        runTest {
+            val cats = listOf(Cat("1", "url"))
+            coEvery { listRepository.getCats() } returns NetworkResult.Success(cats)
+
+            viewModel = ListViewModel(listRepository)
+
+            viewModel.viewState.test {
+                awaitItem() shouldBe ListViewState.Loading
+                awaitItem() shouldBe ListViewState.Success(cats = cats)
+            }
+        }
+
+    @Test
+    fun `should show error state`() =
+        runTest {
+            coEvery { listRepository.getCats() } returns NetworkResult.Error(Exception("Error"))
+
+            viewModel = ListViewModel(listRepository)
+
+            viewModel.viewState.test {
+                awaitItem() shouldBe ListViewState.Loading
+                awaitItem() shouldBe ListViewState.Error(message = "Error")
+            }
+        }
+}
