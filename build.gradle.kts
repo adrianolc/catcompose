9110730591ff348c515554e53a23e883046e68dc
Adriano <adrianolcx@gmail.com>
2025-10-15 17:21:37 +0200

    Run unit test on Pull Request

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
@@ -9,4 +8,12 @@ plugins {
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.compose) apply false
     alias(libs.plugins.kotlinter) apply false
+}
+
+allprojects {
+    tasks.withType<Test> {
+        testLogging {
+            events("skipped", "failed")
+        }
+    }
 }
