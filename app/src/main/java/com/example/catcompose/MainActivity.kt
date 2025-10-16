89ecbbbd201eff6da729518c421fd5591e5035dc
Adriano <adrianolcx@gmail.com>
2025-10-16 11:43:11 +0200

    Enable edge to edge

--- app/src/main/java/com/example/catcompose/MainActivity.kt
+++ app/src/main/java/com/example/catcompose/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.catcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.catcompose.core.designsystem.CatTheme
 import com.example.catcompose.ui.CatApp
@@ -13,6 +14,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
 
         setContent {
             CatTheme {
