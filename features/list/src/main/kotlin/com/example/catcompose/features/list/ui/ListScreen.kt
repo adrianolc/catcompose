788c109f0d92f406edd625ab503caef17b09d93e
Adriano <adrianolcx@gmail.com>
2025-10-16 20:42:12 +0200

    Add Large Top Bar to list screen

--- features/list/src/main/kotlin/com/example/catcompose/features/list/ui/ListScreen.kt
+++ features/list/src/main/kotlin/com/example/catcompose/features/list/ui/ListScreen.kt
@@ -1,12 +1,23 @@
 package com.example.catcompose.features.list.ui
 
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LargeTopAppBar
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.catcompose.features.list.navigation.OnCatClick
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun ListScreen(
     onCatClick: OnCatClick,
@@ -14,19 +25,33 @@ internal fun ListScreen(
     viewModel: ListViewModel = hiltViewModel(),
 ) {
     val viewState by viewModel.viewState.collectAsStateWithLifecycle()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(state = rememberTopAppBarState())
 
-    when (val currentViewState = viewState) {
-        is ListViewState.Error -> {
-            // no-op
-        }
-
-        is ListViewState.Loading -> ListLoading(modifier)
-
-        is ListViewState.Success ->
-            ListContent(
-                modifier = modifier,
-                cats = currentViewState.cats,
-                onCatClick = onCatClick,
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        contentWindowInsets = WindowInsets.safeDrawing,
+        topBar = {
+            LargeTopAppBar(
+                title = { Text("Cats") },
+                scrollBehavior = scrollBehavior,
             )
+        },
+    ) { innerPadding ->
+        val modifierWithPadding = modifier.padding(innerPadding)
+
+        when (val currentViewState = viewState) {
+            is ListViewState.Error -> {
+                // no-op
+            }
+
+            is ListViewState.Loading -> ListLoading(modifier = modifierWithPadding)
+
+            is ListViewState.Success ->
+                ListContent(
+                    modifier = modifierWithPadding,
+                    cats = currentViewState.cats,
+                    onCatClick = onCatClick,
+                )
+        }
     }
 }
