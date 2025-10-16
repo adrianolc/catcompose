2dc6e398c5180cdfb49df127448c0ec871bf0dbe
Adriano <adrianolcx@gmail.com>
2025-10-16 14:35:24 +0200

    Remove Scafold from Root app

--- app/src/main/java/com/example/catcompose/ui/CatApp.kt
+++ app/src/main/java/com/example/catcompose/ui/CatApp.kt
@@ -1,7 +1,5 @@
 package com.example.catcompose.ui
 
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.semantics.semantics
@@ -11,12 +9,11 @@ import com.example.catcompose.navigation.CatNavHost
 
 @Composable
 fun CatApp(modifier: Modifier = Modifier) {
-    Scaffold(
-        modifier = modifier.semantics { testTagsAsResourceId = true },
-    ) { padding ->
-        CatNavHost(
-            navController = rememberNavController(),
-            modifier = modifier.padding(paddingValues = padding),
-        )
-    }
+    CatNavHost(
+        navController = rememberNavController(),
+        modifier =
+            modifier.semantics {
+                testTagsAsResourceId = true
+            },
+    )
 }
