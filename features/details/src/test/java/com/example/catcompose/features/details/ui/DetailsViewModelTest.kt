db0c2749a648ccdc6281963d5c820bb2b6caf019
Adriano <adrianolcx@gmail.com>
2025-10-21 13:41:37 +0200

    Remove saved handler from test and pass catId instead

--- features/details/src/test/java/com/example/catcompose/features/details/ui/DetailsViewModelTest.kt
+++ features/details/src/test/java/com/example/catcompose/features/details/ui/DetailsViewModelTest.kt
@@ -1,15 +1,12 @@
 package com.example.catcompose.features.details.ui
 
-import androidx.lifecycle.SavedStateHandle
 import app.cash.turbine.test
 import com.example.catcompose.core.network.NetworkResult
 import com.example.catcompose.core.test.TestCoroutineRule
-import com.example.catcompose.features.details.navigation.DetailsRoute
 import com.example.catcompose.features.details.repo.Cat
 import com.example.catcompose.features.details.repo.DetailsRepository
 import io.kotest.matchers.shouldBe
 import io.mockk.coEvery
-import io.mockk.every
 import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
 import org.junit.Rule
@@ -21,10 +18,7 @@ class DetailsViewModelTest {
 
     private lateinit var viewModel: DetailsViewModel
     private val detailsRepository: DetailsRepository = mockk(relaxed = true)
-    private val savedStateHandle: SavedStateHandle =
-        mockk {
-            every { get<String>(DetailsRoute.ARG_CAT_ID) } returns "1"
-        }
+    private val catId = "1"
 
     @Test
     fun `should show loading state`() =
@@ -39,8 +33,8 @@ class DetailsViewModelTest {
     @Test
     fun `should load cat details`() =
         runTest {
-            val cat = Cat(id = "1", url = "url", breed = null)
-            coEvery { detailsRepository.getCat("1") } returns NetworkResult.Success(cat)
+            val cat = Cat(id = catId, url = "url", breed = null)
+            coEvery { detailsRepository.getCat(catId) } returns NetworkResult.Success(cat)
 
             viewModel = createViewModel()
 
@@ -53,7 +47,7 @@ class DetailsViewModelTest {
     @Test
     fun `should show error state`() =
         runTest {
-            coEvery { detailsRepository.getCat("1") } returns NetworkResult.Error(Exception("Error"))
+            coEvery { detailsRepository.getCat(catId) } returns NetworkResult.Error(Exception("Error"))
 
             viewModel = createViewModel()
 
@@ -63,5 +57,5 @@ class DetailsViewModelTest {
             }
         }
 
-    private fun createViewModel(): DetailsViewModel = DetailsViewModel(detailsRepository, savedStateHandle)
+    private fun createViewModel(): DetailsViewModel = DetailsViewModel(catId = catId, detailsRepository = detailsRepository)
 }
